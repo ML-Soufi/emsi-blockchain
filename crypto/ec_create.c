33cb36eb6c407a839dba6095cc16c311682dd7ca
SOUFI Mohamed Larbi <soufimohamedlarbi.professional@gmail.com>
2021-05-03 13:56:50 +0000

    crypto last mdf

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,14 +1,13 @@
 #include "hblk_crypto.h"
 
 /**
- * a function that creates a new EC key pair.
- * EC_KEY structure, containing both the public and private keys
+ * ec_create - a function that creates a new EC key pair.
  * Return: EC_KEY * a pointer to EC_KEY, or null on failure
 */
 EC_KEY *ec_create(void)
 {
 EC_KEY *ec_key;
-ec_key = EC_KEY_new_by_curve_name(CURVE_Type);
+ec_key = EC_KEY_new_by_curve_name(EC_CURVE);
 if (ec_key == NULL)
 	return (NULL);
 if (!EC_KEY_generate_key(ec_key))
